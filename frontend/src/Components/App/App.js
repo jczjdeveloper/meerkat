3363e0011bddd0618d84499e72fecc252a1bcdb8
jczjdeveloper <jczjdeveloper@gmail.com>
2018-04-03 01:45:20 +0800

    Added firebase script on index.html

--- frontend/src/Components/App/App.js
+++ frontend/src/Components/App/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 import LogIn from '../LogIn/LogIn';
@@ -12,27 +12,17 @@ class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
-        <Router>
 
+        <Router>
           <Switch>
             <Route exact path="/" component={LogIn}/>
             <Route exact path="/signup" component={SignUp}/>
           </Switch>
-
         </Router>
+
       </MuiThemeProvider>
     );
   }
 }
 
 export default App;
-
-// <div className="App">
-//   <header className="App-header">
-//     <img src={logo} className="App-logo" alt="logo" />
-//     <h1 className="App-title">Welcome to React</h1>
-//   </header>
-//   <p className="App-intro">
-//     To get started, edit <code>src/App.js</code> and save to reload.
-//   </p>
-// </div>
